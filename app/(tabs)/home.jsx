824923a20f7070f412509df57605fb9eb529a55b
Yashindi Bhagya <yashindibhagya@gmail.com>
2025-08-14 22:19:29 +0530

    tailwind setup

---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,17 @@
+import { StyleSheet, View } from 'react-native'
+
+export default function index() {
+  return (
+    <View className="flex-1 bg-neutral-800">
+
+
+    </View >
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1f2937', // equivalent to bg-neutral-800
+  },
+}) 
